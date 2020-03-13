85e820fd5648b560814566b27bd7cdef374ac4e9
Ronald Abreu <ra@bluebirdgroup.ltd>
2020-03-13 20:41:22 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -3,12 +3,11 @@
 def fizzbuzz(int)
   if int % 3 == 0 # if the number int is divisible by 3
     "Fizz" # Go fizz
-  elsif int % 5 == 0 
-    "Buzz" 
-  elsif  int % 15 == 0 
+  elsif int % 5 == 0
+    "Buzz"
+  elsif  int % 15 == 0
     "FizzBuzz"
-  elsif int % 15 || 5 || 3 !== 0 
+  elsif int % 15 || 5 || 3 !== 0
     nil     
   end
-end  
-
+end
